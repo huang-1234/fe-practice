82dc583967b2d9725f968379ce458afea6bd8afa
hsq <137707531@qq.com>
2021-05-23 11:50:06 +0800

    feat: chapter-1 准备工作

---
+++ React/react/src/App.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+// const React = require('react') 
+import {Alert} from './components/alert'
+
+export default function App() {
+  return (
+    <>
+      <Alert />
+    </>
+  )
+}
---
+++ React/react/src/components/alert/index.ts
@@ -0,0 +1,9 @@
+import Alert from './alert';
+
+import './style/index.less';
+
+export default Alert;
+
+export * from './interface';
+
+export { default as Alert } from './alert';
