89e0a738fa2eac02545f687593ec169f762421e2
hsq <137707531@qq.com>
2021-07-23 23:58:19 +0800

    update SkipList

---
+++ TS/src/advance/enum.ts
@@ -0,0 +1,6 @@
+export enum FormElementTypes {
+  checked = "checked"
+}
+
+let order: number[] = [1, 2, 5, 8];
+console.log(order.toString())
--- TS/src/advance/test.js
+++ TS/src/advance/test.js
@@ -1,12 +1,7 @@
-(() => {
-  // console.log(void 0)
-/*   Object.defineProperty(global, "undefined", {
-    writable:true
-  })
-  console.log(global.undefined = 15);
-
-  console.log(Object.getOwnPropertyDescriptor(global, "undefined")); */
-  var a = 15;
-  const obj1 = new Object()
-  console.log(this.a)
-})()
+// {
+//   if ([1,2].toString()) {
+//     console.log('object');
+//   } else {
+//     console.log(1);
+//   }
+// }
