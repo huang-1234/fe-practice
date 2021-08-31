d99dcd0d9a71117decb2b75b94afb2c9673c16c5
hsq <137707531@qq.com>
2021-08-31 23:59:56 +0800

    update algorithm

--- Algorithm/leetcode/75.颜色分类.js
+++ Algorithm/leetcode/75.颜色分类.js
@@ -9,14 +9,38 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// var sortColors = function (nums) {
+//   function swap(nums, x, y) {
+//     const t = nums[x]; nums[x] = nums[y]; nums[y] = t;
+//   }
+//   const len = nums.length
+//   let left = 0, right = len - 1;
+//   let p1 = left;
+//   while (p1 < len && left < right) {
+//     if (nums[p1] === 0) {
+//       swap(nums, p1++, left++);
+//     } else if (nums[p1] === 2) {
+//       swap(nums, p1++, right--)
+//     } else {
+//       p1++;
+//     }
+//   }
+// };
 var sortColors = function (nums) {
-  function swap(nums, x, y) {
-    const t = nums[x]; nums[x] = nums[y]; nums[y] = t;
+  const len = nums.length
+  let count = new Array(3).fill(0);
+  for (let i = 0;i < len;i++) {
+    count[nums[i]]++;
   }
-  let left = 0, right = nums.length-1;
-  let p1 = left, p2 = right;
-  while (left <= right) {
-    if(nums[])
+  let index = 0;
+  for (let i = 0;i !== count[0];i++) {
+    nums[index++] = 0;
+  }
+  for (let i = 0;i !== count[1];i++) {
+    nums[index++] = 1;
+  }
+  for (let i = 0;i !== count[2];i++) {
+    nums[index++] = 2;
   }
 };
 // @lc code=end
