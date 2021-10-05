13f0d27a70b719fb6384bc6801fe7bb31741598c
hsq <137707531@qq.com>
2021-10-05 23:58:35 +0800

    update al

--- Algorithm/leetcode/test/string/toUpperCase.js
+++ Algorithm/leetcode/test/string/toUpperCase.js
@@ -5,19 +5,27 @@
  * @param {number} k
  * @return {string}
  */
-var licenseKeyFormatting = function(str, k) {
-  const UpperReverseStr = str.toUpperCase().split("-").reverse().join("").split("")
+var licenseKeyFormatting = function (str, k) {
+  // const UpperReverseStr = str.toUpperCase()
+  
+  for (let i = 0, len = str.length;i < len;i++) {
+    if( str[i])
+  }
+    // .split("-").reverse().join("").split("")
+  console.log(UpperReverseStr)
   const UrsLen = UpperReverseStr.length;
   const resStr = []
   const klen = UrsLen / k | 0;
   let i = 0;
-  for (;i < klen*k; i+=k){
-    resStr.push(UpperReverseStr.slice(i,i+k).join(""))
+  for (;i < klen * k;i += k) {
+    resStr.push(UpperReverseStr.slice(i, i + k).join(""))
   }
   if (i < UrsLen) {
-    resStr.push(UpperReverseStr.slice(i,UrsLen ).join(""))
+    resStr.push(UpperReverseStr.slice(i, UrsLen).join(""))
   }
   return resStr.reverse().join("-")
 };
 
-licenseKeyFormatting(`5F3Z-2e-9-w`, 2)
+const str = "5F3Z-2e-9-w";
+const k = 4
+console.log(licenseKeyFormatting(str, k))
