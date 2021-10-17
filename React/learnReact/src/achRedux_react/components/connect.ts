0541f01eadfb19487567d49adc3d1b19ee35589e
hsq <137707531@qq.com>
2021-10-17 23:47:08 +0800

    update al

---
+++ React/learnReact/src/achRedux_react/components/connect.ts
@@ -0,0 +1,44 @@
+import React, {Component} from "react"
+
+export const connect = (mapStateToProps: (arg0: any, arg1: Readonly<{}> & Readonly<{ children?: React.ReactNode }>) => any, mapDispatchToProps: (arg0: any, arg1: Readonly<{}> & Readonly<{ children?: React.ReactNode }>) => any) => (WrappedComponent: any) => {
+  class Connect extends Component {
+    static contextTypes = {
+      store: PropTypes.object
+    }
+
+    constructor () {
+      super()
+      this.state = {
+        allProps: {}
+      }
+    }
+
+    componentWillMount () {
+      const { store } = this.context
+      this._updateProps()
+      store.subscribe(() => this._updateProps())
+    }
+
+    _updateProps () {
+      const { store } = this.context
+      let stateProps = mapStateToProps
+        ? mapStateToProps(store.getState(), this.props)
+        : {} // 防止 mapStateToProps 没有传入
+      let dispatchProps = mapDispatchToProps
+        ? mapDispatchToProps(store.dispatch, this.props)
+        : {} // 防止 mapDispatchToProps 没有传入
+      this.setState({
+        allProps: {
+          ...stateProps,
+          ...dispatchProps,
+          ...this.props
+        }
+      })
+    }
+
+    render () {
+      return <WrappedComponent {...this.state.allProps} />
+    }
+  }
+  return Connect
+}
---
+++ TS/src/basic/override/function.ts
@@ -0,0 +1,11 @@
+function add(a: string, b: string): string{
+  console.log(a+b)
+  return a + b
+}
+
+function add(a: number, b: number): number{
+  console.log(a+b)
+  return a+b
+}
+
+add(8,5)
