fc3cf2816fc8a919fd99f8db8fd3cb3608049d90
hsq <137707531@qq.com>
2022-01-01 14:52:43 +0800

    update es

--- JavaScript/es/scope/closure.js
+++ JavaScript/es/scope/closure.js
@@ -10,13 +10,13 @@
 // 解决方法有 3 种
 
 // 第一种，使用`立即执行函数`方式
-// for (var i = 1;i < 5;i++) {
-//   (function(j){
-//     setTimeout(function timer() {
-//       console.log(j)
-//     }, j * 1000)
-//   })(i)
-// }
+for (var i = 1;i < 5;i++) {
+  (function(j){
+    setTimeout(function timer() {
+      console.log(j)
+    }, j * 1000)
+  })(i)
+}
 
 // // 第二种，使用 ES6 的`let`
 // for (let i = 1;i < 5;i++) {
