578ac482302811351506fb035251aa9e0fca1161
huangsq <137707531@qq.com>
2023-06-03 14:18:40 +0800

    feat: ac

--- leetcode/dp/1143.js
+++ leetcode/dp/1143.js
@@ -1,4 +1,18 @@
-const [lenA, lenB] = [5, 10]
+const [lenA, lenB] = [2, 3]
 
-const dp = new Array(lenA).fill(new Array(lenB).fill(0));
-console.log(dp)
+const dp1 = new Array(lenA + 1).fill(new Array(lenB + 1).fill(0));
+const dp2 = new Array(lenA + 1).fill(0).map(() => new Array(lenB + 1).fill(0));
+// console.log(dp)
+// console.log(dp2)
+
+dp1.forEach((d, i) => {
+  d.forEach((c, j) => {
+    console.log(c + (i + 1) * j + (j + 1))
+  })
+})
+console.log('============')
+dp2.forEach((d, i) => {
+  d.forEach((c, j) => {
+    console.log(c + (i + 1) * j + (j + 1))
+  })
+})
