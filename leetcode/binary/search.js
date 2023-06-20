18da2a340d86526cbcb188bc659c8767e96291cb
huangshuiqing <huangshuiqing.0@bytedance.com>
2023-06-20 13:54:00 +0800

    feat: add right_bound

--- leetcode/binary/search.js
+++ leetcode/binary/search.js
@@ -42,18 +42,15 @@
   }
 
 
-  let td = [1, 3, 3, 3, 6, 5, 10, 10, 10, 13, 13, 14, 18, 99, 99, 99, 99, 100, 100];
 
-  console.log(left_bound(td, 99), td.length, '===');
-  console.log(right_bound(td, 99))
 }
 
 {
   const left_bound = function (nums, target) {
     // 搜索区间为 [left, right]
-    var left = 0, right = nums.length - 1;
+    let left = 0, right = nums.length - 1;
     while (left <= right) {
-      var mid = left + Math.floor((right - left) / 2);
+      const mid = left + Math.floor((right - left) / 2);
       // 如果要寻找左侧边界，有两种情况需要更新 right
       // 当 nums[mid] == target，因为要找到最左边的下标，所以要把 right 更新到 mid - 1
       // 当 nums[mid] > target，因为 target 只可能出现在左侧，所以要把 right 更新到 mid - 1
@@ -67,4 +64,31 @@
       }
     }
   }
+  // 注意：javascript 代码由 chatGPT🤖 根据我的 java 代码翻译，旨在帮助不同背景的读者理解算法逻辑。
+  // 本代码还未经过力扣测试，仅供参考，如有疑惑，可以参照我写的 java 代码对比查看。
+
+  const right_bound = function (nums, target) {
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+      const mid = left + Math.floor((right - left) / 2);
+      if (nums[mid] < target) {
+        left = mid + 1;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else if (nums[mid] == target) {
+        // 这里改成收缩左侧边界即可
+        left = mid + 1;
+      }
+    }
+    // 最后改成返回 left - 1
+    if (left - 1 < 0 || left - 1 >= nums.length) {
+      return -1;
+    }
+    return nums[left - 1] == target ? (left - 1) : -1;
+  };
+  let td = [1, 3, 3, 3, 6, 5, 10, 10, 10, 13, 13, 14, 18, 99, 99, 99, 99, 100, 100];
+
+  console.log(left_bound(td, 99), td.length, '===');
+  console.log(right_bound(td, 99))
+
 }
