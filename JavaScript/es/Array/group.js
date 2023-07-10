a1b240daaf876d5036897075e14b44878b8efecb
huangshuiqing <huangshuiqing.0@bytedance.com>
2023-07-11 01:17:52 +0800

    feat: ac

---
+++ JavaScript/es/Array/group.js
@@ -0,0 +1,26 @@
+const inventory = [
+  { name: "asparagus", type: "vegetables", quantity: 5 },
+  { name: "bananas", type: "fruit", quantity: 0 },
+  { name: "goat", type: "meat", quantity: 23 },
+  { name: "cherries", type: "fruit", quantity: 5 },
+  { name: "fish", type: "meat", quantity: 22 },
+];
+const result = inventory.group(({ type }) => type);
+console.log(result)
+/* 结果是：
+{
+  vegetables: [
+    { name: 'asparagus', type: 'vegetables', quantity: 5 },
+  ],
+  fruit: [
+    { name: "bananas", type: "fruit", quantity: 0 },
+    { name: "cherries", type: "fruit", quantity: 5 }
+  ],
+  meat: [
+    { name: "goat", type: "meat", quantity: 23 },
+    { name: "fish", type: "meat", quantity: 22 }
+  ]
+}
+*/
+
+// TypeError: inventory.group is not a function
