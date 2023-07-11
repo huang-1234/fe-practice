0259a2849406e95f40c76829858651c5aa463114
huangshuiqing <huangshuiqing.0@bytedance.com>
2023-07-11 10:42:01 +0800

    feat: add array polyfill

--- JavaScript/es/Array/group.js
+++ JavaScript/es/Array/group.js
@@ -5,6 +5,33 @@ const inventory = [
   { name: "cherries", type: "fruit", quantity: 5 },
   { name: "fish", type: "meat", quantity: 22 },
 ];
+
+Array.prototype.group = function (callback, thisArg) {
+  const result = Object.create(null);
+  for (const [index, elem] of this.entries()) {
+    const groupKey = callback.call(thisArg, elem, index, this);
+    if (! (groupKey in result)) {
+      result[groupKey] = [];
+    }
+    result[groupKey].push(elem);
+  }
+  return result;
+};
+Array.prototype.groupToMap = function (callback, thisArg) {
+  const result = new Map();
+  for (const [index, elem] of this.entries()) {
+    const groupKey = callback.call(thisArg, elem, index, this);
+    let group = result.get(groupKey);
+    if (group === undefined) {
+      group = [];
+      result.set(groupKey, group);
+    }
+    group.push(elem);
+  }
+  return result;
+};
+
+
 const result = inventory.group(({ type }) => type);
 console.log(result)
 /* 结果是：
