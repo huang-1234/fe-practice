2f8d33615461c14dc4efcb8812621ad0c18603bd
huangsq <137707531@qq.com>
2025-07-25 01:24:06 +0800

    feat: add lengthOfLIS

--- Algorithm/leetcode/300.最长递增子序列.js
+++ Algorithm/leetcode/300.最长递增子序列.js
@@ -9,8 +9,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function(nums) {
 
+var lengthOfLIS = function(nums) {
+	if (!nums || nums.length === 0) return 0;
+  const dp = new Array(nums.length).fill(1);
+  let maxRes = 1;
+	for (let i = 1; i < nums.length; i++) {
+		for (let j = 0; j < i; j++) {
+			if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1); // dp[i] = Math.max(dp[i], dp[j] + 1);
+        maxRes = Math.max(maxRes, dp[i]);
+			}
+		}
+	}
+	return maxRes;
 };
 // @lc code=end
 
